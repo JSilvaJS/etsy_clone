9cf9a80acec4e91db0e51424c2a566e6b5dd181a
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-13 14:51:08 -0400

    added html, css, and js for js_content and other elements... still need bottom left side bar and other elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,32 @@
 // Javascript Entry Point
+
+import etsyData from './items';
+import $ from 'jquery';
+
+var etsyProducts = etsyData.results;
+
+var etsyProdHtml = etsyProducts.map(function(product) {
+	var images = product.Images[0].url_170x135;
+	var title =  product.title;
+	// var seller = etsyProducts.
+	var price = product.price;
+	var shopName = product.Shop.shop_name;
+
+	return `
+	  <div class="boxes_wrapper">
+      <div class="image_box">
+        <img src="${images}" alt="${title}">
+      </div>
+      <div class="price">$${price}</div>
+      <div class="shopName">${shopName}</div>
+      <div class="title">${title}</div>
+    </div>
+  `;
+});
+
+
+$('.js_content').append(etsyProdHtml);
+
+
+
+
